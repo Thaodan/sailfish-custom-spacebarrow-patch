bd47925b472be28e4357ee1086d18147a5920833
Björn Bidar <theodorstormgrade@gmail.com>
2019-02-01 00:47:37 +0100

    Update project.mk with the new Makefile vars

--- project.mk
+++ project.mk
@@ -1,4 +1,8 @@
 NAME = Custom-Spacebarrow
 SUMMARY = "A patch to add ! , ? to SpacebarRow as accents"
+DESCRIPTION := $(SUMMARY)
 LICENSE=GPL
 VER=0.1
+MAINTAINER=Thaodan
+# see https://github.com/sailfishos-patches/patchmanager#categories
+CATEGORY=others
